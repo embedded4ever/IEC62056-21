307a98d20ef9f1f1bcd589925ddeff4eec0e5e59
Volkan Unal <engineeringvolkan@hotmail.com>
2020-04-06 11:33:45 +0300

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -95,9 +95,8 @@ void HAL_UART_RxCpltCallback (UART_HandleTypeDef * huart)
 
 void HAL_UART_TxCpltCallback (UART_HandleTypeDef *huart)
 {
-	
 	if (huart->Instance == USART2)  
-  {
+	{
 		systick_tick = 0;
 		buffer_index = 0;
 		MAX485_RECV
